4f9f0ad444e4a70e9597099e640be9a39bf5b70a
pookmish <pookmish@stanford.edu>
2020-04-23 09:53:03 -0700

    Consolidated codeception suite config into main file so functional tests can run (#8)

--- blt/src/Blt/Plugin/Commands/GryphonTestCommands.php
+++ blt/src/Blt/Plugin/Commands/GryphonTestCommands.php
@@ -4,6 +4,7 @@ namespace Example\Blt\Plugin\Commands;
 
 use Acquia\Blt\Robo\BltTasks;
 use Acquia\Blt\Robo\Exceptions\BltException;
+use Robo\ResultData;
 use Symfony\Component\Console\Output\OutputInterface;
 
 /**
@@ -45,9 +46,18 @@ class GryphonTestCommands extends BltTasks {
    *   Result of the test.
    */
   public function runCodeceptionTests($test_key) {
+    $failed_test = NULL;
     if ($test = $this->getConfigValue('tests.codeception.' . $test_key)) {
-      return $this->runCodeceptionTestSuite($test['suite'], $test['directory']);
+      foreach ($test['suites'] as $suite) {
+        $this->say("Running <comment>$suite</comment> Tests.");
+        $test_result = $this->runCodeceptionTestSuite($suite, $test['directory']);
+
+        if (!$test_result->wasSuccessful()) {
+          $failed_test = $test_result;
+        }
+      }
     }
+    return $failed_test ?: $test_result;
   }
 
   /**
@@ -58,17 +68,21 @@ class GryphonTestCommands extends BltTasks {
    * @param string $test_directory
    *   Directory to codeception tests.
    *
-   * @return \Robo\Result
+   * @return \Robo\Result|\Robo\ResultData
    *   Result of the test.
    */
   protected function runCodeceptionTestSuite($suite, $test_directory) {
+    if (!file_exists("$test_directory/$suite/")) {
+      return new ResultData(ResultData::EXITCODE_OK, 'No tests to execute for suite ' . $suite);
+    }
+
     $root = $this->getConfigValue('repo.root');
-    if (!file_exists("$root/tests/codeception/$suite.suite.yml")) {
+    if (!file_exists("$root/tests/codeception.yml")) {
       $this->taskFilesystemStack()
-        ->copy("$root/tests/codeception/$suite.suite.dist.yml", "$root/tests/codeception/$suite.suite.yml")
+        ->copy("$root/tests/codeception.dist.yml", "$root/tests/codeception.yml")
         ->run();
       $this->getConfig()
-        ->expandFileProperties("$root/tests/codeception/$suite.suite.yml");
+        ->expandFileProperties("$root/tests/codeception.yml");
     }
 
     $new_test_dir = "$root/tests/codeception/$suite/" . date('Ymd-Hi');
@@ -83,6 +97,7 @@ class GryphonTestCommands extends BltTasks {
       ->arg($suite)
       ->option('steps')
       ->option('config', 'tests', '=')
+      ->option('override', "paths: output: ../artifacts/$suite", '=')
       ->option('html')
       ->option('xml');
 
@@ -96,22 +111,15 @@ class GryphonTestCommands extends BltTasks {
     // test directory.
     $this->taskDeleteDir($new_test_dir)->run();
 
-    $codeception_config = $this->taskExec('vendor/bin/codecept')
-      ->arg('config:validate')
-      ->option('config', 'tests', '=')
-      ->printOutput(FALSE)
-      ->run()
-      ->getMessage();
-
     // Delete the failed file because codeception will try to look for the file
     // that failed again on the next run. Since we have temporary test
     // directories we don't want to save that data.
-    preg_match_all('/output => (.*)$/m', $codeception_config, $preg_matches);
-    if (!empty($preg_matches[1][0]) && is_dir("$root/tests/{$preg_matches[1][0]}")) {
+    foreach (glob("$root/artifacts/*/failed") as $file) {
       $this->taskFilesystemStack()
-        ->remove("$root/tests/{$preg_matches[1][0]}/failed")
+        ->remove($file)
         ->run();
     }
+
     return $test_result;
   }
 
