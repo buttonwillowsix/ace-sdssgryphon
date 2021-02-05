897b83fd3fa0dc58f8cbceaae82a70a1a8a4808b
pookmish <pookmish@stanford.edu>
2021-02-05 11:16:25 -0800

    Fixed the order of processes for updating certs (#17)
    * Fixed the order of processes for updating certs
    
    * updated compser dependencies
    
    * Added render_clean to ignore twig validate
    
    * Make John happy
    
    * Updated dependency

--- blt/src/Blt/Plugin/Commands/GryphonCommands.php
+++ blt/src/Blt/Plugin/Commands/GryphonCommands.php
@@ -281,18 +281,28 @@ class GryphonCommands extends BltTasks {
     $this->say(var_export($api->addCert($environment, $cert, $key, $intermediate, $label), TRUE));
 
     $certs = $api->getCerts($environment);
+
+    $delete_cert_ids = [];
     foreach ($certs['_embedded']['items'] as $cert) {
-      // Find the cert we just created and activate it.
+      // Find the cert we just created so we can activate it.
       if ($cert['label'] == $label) {
-        $this->say(var_export($api->activateCert($environment, $cert['id']), TRUE));
+        $activate_cert_id = $cert['id'];
         continue;
       }
 
-      // Remove any certs that are outdated.
+      // Find all the outdated certs.
       if (strtotime($cert['expires_at']) < time()) {
-        $this->say(var_export($api->removeCert($environment, $cert['id']), TRUE));
+        $delete_cert_ids[] = $cert['id'];
       }
     }
+
+    // Activate the new cert before we delete the old certs. This prevents the
+    // possibility of deleting an active cert.
+    $this->say(var_export($api->activateCert($environment, $activate_cert_id), TRUE));
+    foreach ($delete_cert_ids as $cert_id) {
+      $this->say(var_export($api->removeCert($environment, $cert_id), TRUE));
+    }
+
     // Cleanup the local certs files.
     $this->taskDeleteDir($local_cert_dir)->run();
   }
