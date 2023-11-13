a304ea9e92edd7cf21d6c32413ba985fa6163fc3
Joe Gilliland-Lloyd <6943710+joegl@users.noreply.github.com>
2023-11-13 10:29:08 -0600

    SDSS-1092: Removed JS compiling in sdss_subtheme (#306)
    * SDSS-1092: Removed JS compiling in sdss_subtheme.

--- docroot/profiles/sdss/sdss_profile/themes/sdss_subtheme/dist/js/scripts.js
+++ docroot/profiles/sdss/sdss_profile/themes/sdss_subtheme/dist/js/scripts.js
@@ -1,29 +1,10 @@
-/******/ (function() { // webpackBootstrap
-/******/ 	"use strict";
-var __webpack_exports__ = {};
-/**
- * Behavior Example that works with Webpack.
- *
- * @see: https://www.npmjs.com/package/drupal-behaviors-loader
- *
- * Webpack wraps everything in enclosures and hides the global variables from
- * scripts so special handling is needed.
- */
-
-/* unused harmony default export */ var __WEBPACK_DEFAULT_EXPORT__ = ({
-  // Attach Drupal Behavior.
-  attach: function attach(context, settings) {
-    // console.log("Attached.");
-
-    (function ($) {
+(function ($, Drupal, once) {
+  'use strict';
+  Drupal.behaviors.sdss_subtheme = {
+    attach: function (context, settings) {
       // Add search link button to navigation.
       $('#block-sdss-subtheme-main-navigation').after('<a href="/search" id="sdss-button--search-link" class="su-site-search__submit"><span class="visually-hidden">Search</span></a>');
-    })(jQuery);
-  },
-  // Detach Example.
-  detach: function detach() {
-    // console.log("Detached.");
-  }
-});
-/******/ })()
-;
+    }
+  };
+
+})(jQuery, Drupal, once);
